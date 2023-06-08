d12a9cee1b1ff95c8c1b327cec76206c7f1e97da
Jinchen.Z <115627891+Jinchen2028@users.noreply.github.com>
2023-06-08 19:59:48 +0800

    EMT！

--- archs/utils/_tool.py
+++ archs/utils/_tool.py
@@ -33,12 +33,11 @@ class Scale(nn.Module):
 class PixelMixer(nn.Module):
     r"""Pixel Mixer.
 
-    Args:
-        planes (int):
-        mix_margin (int):
+        Args:
+            planes (int):
+            mix_margin (int):
 
-    """
-
+        """
     def __init__(self, planes: int, mix_margin: int = 1) -> None:
         super(PixelMixer, self).__init__()
 
@@ -46,9 +45,9 @@ class PixelMixer(nn.Module):
 
         self.planes = planes
         self.mix_margin = mix_margin
+        self.mask = nn.Parameter(torch.zeros((self.planes, 1, mix_margin * 2 + 1, mix_margin * 2 + 1)),
+                                 requires_grad=False)
 
-        self.mask = torch.zeros((self.planes, 1, mix_margin * 2 + 1, mix_margin * 2 + 1),
-                                requires_grad=False)
         self.mask[3::5, 0, 0, mix_margin] = 1.
         self.mask[2::5, 0, -1, mix_margin] = 1.
         self.mask[1::5, 0, mix_margin, 0] = 1.
@@ -57,9 +56,10 @@ class PixelMixer(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         m = self.mix_margin
-        return f.conv2d(input=f.pad(x, pad=(m, m, m, m), mode='circular'),
-                        weight=self.mask, bias=None, stride=(1, 1), padding=(0, 0),
-                        dilation=(1, 1), groups=self.planes)
+        x = f.conv2d(input=f.pad(x, pad=(m, m, m, m), mode='circular'),
+                     weight=self.mask, bias=None, stride=(1, 1), padding=(0, 0),
+                     dilation=(1, 1), groups=self.planes)
+        return x
 
 
 class ChannelMixer(nn.Module):
