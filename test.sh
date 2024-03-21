141a92bd369cb683dba0df0af717627292686a42
jinpeng.505-Server-3 <jinpeng.s@outlook.com>
2024-03-21 14:42:02 +0800

    res(test.sh): update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,5 @@
 # ESWT
-python test.py -expe_opt options/expe/ESWT/ESWT-12-12_LSR.yml -task_opt options/task/RSSR_x4.yml
-python test.py -expe_opt options/expe/ESWT/ESWT-6-24_LSR.yml -task_opt options/task/RSSR_x4.yml
+python test.py -expe_opt options/expe/ESWT/ESWT-24-6_LSR.yml -task_opt options/task/RSSR_x4.yml
 
 # ELAN
 python test.py -expe_opt options/expe/ELAN/ELAN_LSR.yml -task_opt options/task/RSSR_x4.yml
