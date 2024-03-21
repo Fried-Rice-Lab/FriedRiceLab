ee909e2e9fc48db3967a609cf51b3c180918c54d
Jinpeng Shi <login@jpshi.me>
2024-03-21 15:40:50 +0800

    fix a bug

---
+++ infer.sh
@@ -0,0 +1,16 @@
+# ESWT
+python test.py -expe_opt options/repr/ESWT/ESWT-24-6_LSR.yml -task_opt options/task/RSSR_x4.yml
+
+# ELAN
+python test.py -expe_opt options/repr/ELAN/ELAN_LSR.yml -task_opt options/task/RSSR_x4.yml
+
+# FDIWN
+python test.py -expe_opt options/repr/FDIWN/FDIWN_LSR.yml -task_opt options/task/RSSR_x4.yml
+
+# HNCT
+python test.py -expe_opt options/repr/HNCT/HNCT_LSR.yml -task_opt options/task/RSSR_x4.yml
+
+# LAPAR
+python test.py -expe_opt options/repr/LAPAR/LAPAR-A_LSR.yml -task_opt options/task/RSSR_x4.yml
+python test.py -expe_opt options/repr/LAPAR/LAPAR-B_LSR.yml -task_opt options/task/RSSR_x4.yml
+python test.py -expe_opt options/repr/LAPA/LAPAR-C_LSR.yml -task_opt options/task/RSSR_x4.yml
