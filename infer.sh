942983d6db077d86c50c337fcf6fc85a553a7692
Jinpeng Shi <login@jpshi.me>
2024-03-21 14:17:01 +0800

    res: infer.sh -> test.sh

--- infer.sh
+++
@@ -1,1 +0,0 @@
-python test.py -expe_opt options/expe/ELAN/ELAN_LSR.yml -task_opt options/task/RSSR_x4.yml
---
+++ test.sh
@@ -0,0 +1,9 @@
+# ESWT
+python test.py -expe_opt options/expe/ESWT/ESWT-12-12_LSR.yml -task_opt options/task/RSSR_x4.yml
+python test.py -expe_opt options/expe/ESWT/ESWT-6-24_LSR.yml -task_opt options/task/RSSR_x4.yml
+
+# ELAN
+python test.py -expe_opt options/expe/ELAN/ELAN_LSR.yml -task_opt options/task/RSSR_x4.yml
+
+# FDIWN
+python test.py -expe_opt options/expe/FDIWN/FDIWN_LSR.yml -task_opt options/task/RSSR_x4.yml
